0242021b6fc6b2e4e2649d2d0fb94a88c4e77632
Nguyễn Quang Minh (NQM) <94773751+nqmgaming@users.noreply.github.com>
2023-07-18 01:08:28 +0700

    ok

--- app/src/main/java/com/nqmgaming/assignment_minhnqph31902/Adapter/SlideAdapter.java
+++ app/src/main/java/com/nqmgaming/assignment_minhnqph31902/Adapter/SlideAdapter.java
@@ -14,12 +14,16 @@ import androidx.viewpager.widget.PagerAdapter;
 import com.nqmgaming.assignment_minhnqph31902.R;
 
 public class SlideAdapter extends PagerAdapter {
+
+    //create context
     Context context;
 
+    //create constructor
     public SlideAdapter(Context context) {
         this.context = context;
     }
 
+    //Create 3 int images for todo app
     public int[] slide_images = {
             R.drawable.welcomeone,
             R.drawable.welcometwo,
@@ -40,10 +44,12 @@ public class SlideAdapter extends PagerAdapter {
             "You can delete task by click on the delete button"
     };
 
+    //get count of slide_headings
     @Override
     public int getCount() {
         return slide_headings.length;
     }
+
 
     @Override
     public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
@@ -54,17 +60,21 @@ public class SlideAdapter extends PagerAdapter {
     @Override
     public Object instantiateItem(@NonNull ViewGroup container, int position) {
 
+        //create layout inflater
         LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
         View view = layoutInflater.inflate(R.layout.slide_layout, container, false);
 
+        //mapping variables with view
         ImageView slideImageView = view.findViewById(R.id.imgSlide);
         TextView txtTitle = view.findViewById(R.id.txtTitle);
         TextView txtDescription = view.findViewById(R.id.txtDescription);
 
+        //set image, title, description for each slide
         slideImageView.setImageResource(slide_images[position]);
         txtTitle.setText(slide_headings[position]);
         txtDescription.setText(slide_descs[position]);
 
+        //add view to container
         container.addView(view);
 
         return view;
@@ -72,6 +82,8 @@ public class SlideAdapter extends PagerAdapter {
 
     @Override
     public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        //remove view from container
         container.removeView((LinearLayout) object);
+
     }
 }
--- app/src/main/java/com/nqmgaming/assignment_minhnqph31902/DAO/UserDAO.java
+++ app/src/main/java/com/nqmgaming/assignment_minhnqph31902/DAO/UserDAO.java
@@ -11,87 +11,153 @@ import com.nqmgaming.assignment_minhnqph31902.DataBase.DataBaseTodoAppHelper;
 
 public class UserDAO {
 
+    //create variables
     DataBaseTodoAppHelper dataBaseTodoAppHelper;
     SQLiteDatabase sqLiteDatabase;
 
+    //constructor
     public UserDAO(Context context) {
+
         dataBaseTodoAppHelper = new DataBaseTodoAppHelper(context);
         sqLiteDatabase = dataBaseTodoAppHelper.getWritableDatabase();
+
     }
 
     //Insert new user return long
     public long insertUser(UserDTO userDTO) {
+
+        //create result variable
         long result = -1;
 
+        //create content values
         ContentValues contentValues = new ContentValues();
+
+        //put data to content values
         contentValues.put("username", userDTO.getUsername());
         contentValues.put("email", userDTO.getEmail());
         contentValues.put("password", userDTO.getPassword());
         contentValues.put("firstname", userDTO.getFirstname());
         contentValues.put("lastname", userDTO.getLastname());
 
+        //try catch
         try {
+
+            //insert data to database
             result = sqLiteDatabase.insertOrThrow("user_table", null, contentValues);
+
         } catch (SQLException e) {
+
+            //print error
             e.printStackTrace();
+
         }
 
+        //return result
         return result;
     }
 
     //Delete user return int
     public int deleteUser(UserDTO userDTO) {
+
+        //create result variable
         int result = -1;
+
+        //create condition
         String[] condition = new String[]{
                 String.valueOf(userDTO.getId())
         };
 
+        //try catch
         try {
+
+            //delete user
             result = sqLiteDatabase.delete("user_table", "id = ?", condition);
+
         } catch (SQLException e) {
+
+            //print error
             e.printStackTrace();
+
         }
+
+        //return result
         return result;
+
     }
 
     //Update user return int
     public int updateUser(UserDTO userDTO) {
+
+        //create result variable
         int result = -1;
+
+        //create content values
         ContentValues contentValues = new ContentValues();
+
+        //put data to content values
         contentValues.put("username", userDTO.getUsername());
         contentValues.put("email", userDTO.getEmail());
         contentValues.put("password", userDTO.getPassword());
         contentValues.put("firstname", userDTO.getFirstname());
         contentValues.put("lastname", userDTO.getLastname());
 
+        //create condition
         String[] condition = new String[]{String.valueOf(userDTO.getId())};
 
+        //try catch
         try {
+
+            //update user
             result = sqLiteDatabase.update("user_table", contentValues, "id = ?", condition);
+
         } catch (SQLException e) {
+
+            //print error
             e.printStackTrace();
+
         }
 
+        //return result
         return result;
+
     }
 
     //Check user exist return boolean
     public boolean checkUserExist(String username) {
+
+        //create result variable
         boolean result = false;
+
+        //create condition
         String[] condition = new String[]{
                 username
         };
 
+        //try catch
         try {
+
+            //create cursor
             Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM user_table WHERE username = ?", condition);
+
+            //check cursor if move to first return true
             if (cursor.moveToFirst()) {
+
+                //set result to true if cursor move to first because user exist
                 result = true;
+
             }
+
+            //close cursor because we don't need it anymore
             cursor.close();
+
         } catch (SQLException e) {
+
+            //print error
             e.printStackTrace();
+
         }
 
+        //return result
         return result;
     }
 
@@ -116,97 +182,199 @@ public class UserDAO {
     }
 
     //check account match return boolean
-    public boolean checkAccount(String email, String password){
+    public boolean checkAccount(String email, String password) {
+
+        //create result variable
         boolean result = false;
+
+        //create condition
         String[] condition = new String[]{
                 email, password
         };
-      try {
+
+        //try catch
+        try {
+
+            //create cursor
             Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM user_table WHERE email = ? AND password = ?", condition);
+
+            //check cursor if move to first return true
             if (cursor.moveToFirst()) {
+
+                //set result to true if cursor move to first because account match
                 result = true;
+
             }
+
+            //close cursor because we don't need it anymore
             cursor.close();
-      }catch (SQLException e){
-          e.printStackTrace();
-      }
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+
+        }
+
+        //return result
         return result;
     }
 
     //check username and email match return boolean
-    public boolean checkUsernameAndEmail(String username, String email){
+    public boolean checkUsernameAndEmail(String username, String email) {
+
+        //create result variable
         boolean result = false;
+
+        //create condition
         String[] condition = new String[]{
                 username, email
         };
-      try {
+
+        //try catch
+        try {
+
+            //create cursor
             Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM user_table WHERE username = ? AND email = ?", condition);
+
+            //check cursor if move to first return true
             if (cursor.moveToFirst()) {
                 result = true;
             }
+
+            //close cursor because we don't need it anymore
             cursor.close();
-      }catch (SQLException e){
-          e.printStackTrace();
-      }
+
+        } catch (SQLException e) {
+
+            //print error
+            e.printStackTrace();
+
+        }
+
+        //return result
         return result;
+
     }
 
     //check username and email after return UserDTO
-    public UserDTO checkUsernameAndEmailAfter(String username, String email){
+    public UserDTO checkUsernameAndEmailAfter(String username, String email) {
+
+        //create userDTO variable
         UserDTO userDTO = null;
+
+        //create condition
         String[] condition = new String[]{
                 username, email
         };
-      try {
+        try {
+
+            //create cursor
             Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM user_table WHERE username = ? AND email = ?", condition);
+
+            //check cursor if move to first set data to userDTO
             if (cursor.moveToFirst()) {
+
+                //create userDTO
                 userDTO = new UserDTO();
+
+                //set data to userDTO
                 userDTO.setId(cursor.getInt(0));
                 userDTO.setUsername(cursor.getString(1));
                 userDTO.setEmail(cursor.getString(2));
                 userDTO.setPassword(cursor.getString(3));
                 userDTO.setFirstname(cursor.getString(4));
                 userDTO.setLastname(cursor.getString(5));
+
             }
+
+            //close cursor because we don't need it anymore
             cursor.close();
-      }catch (SQLException e){
-          e.printStackTrace();
-      }
+
+        } catch (SQLException e) {
+
+            //print error
+            e.printStackTrace();
+
+        }
+
+        //return userDTO
         return userDTO;
+
     }
 
     //check if password match return boolean
-    public boolean checkPassword(String password){
+    public boolean checkPassword(String password) {
+
+        //create result variable
         boolean result = false;
+
+        //create condition
         String[] condition = new String[]{
                 password
         };
-      try {
+
+        //try catch
+        try {
+
+            //create cursor
             Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM user_table WHERE password = ?", condition);
+
+            //check cursor if move to first return true
             if (cursor.moveToFirst()) {
+
+                //set result to true if cursor move to first because password match
                 result = true;
+
             }
+
             cursor.close();
-      }catch (SQLException e){
-          e.printStackTrace();
-      }
+
+        } catch (SQLException e) {
+
+            //print error
+            e.printStackTrace();
+
+        }
+
+        //return result
         return result;
     }
 
     //Get id by username and email return int
     public int getIdByUsernameAndEmail(String username, String email) {
+
+        //create result variable
         int result = -1;
+
+        //create condition
         String[] condition = new String[]{username, email};
+
+        //try catch
         try {
+
+            //create cursor
             Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM user_table WHERE username = ? AND email = ?", condition);
+
+            //check cursor if move to first return true
             if (cursor.moveToFirst()) {
+
+                //set result is id
                 result = cursor.getInt(0);
             }
+
+            //close cursor because we don't need it anymore
             cursor.close();
+
         } catch (SQLException e) {
+
+            //print error
             e.printStackTrace();
+
         }
+
+        //return result
         return result;
+
     }
 
 
